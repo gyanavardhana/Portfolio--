a75c76f4b77366615ac4a84966de1b227d3399ec
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-05-11 15:25:32 +0530

    Update 1 Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,10 +29,10 @@ const Contact = (props) => {
 
         emailjs
             .send(
-                'service_ktubylm',
-                'template_dlpqvar,
+                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
                 data,
-                'gvXZTE_FzQ3gY741O'
+                import.meta.env.VITE_APP_EMAILJS_PUBLIC_API
             )
             .then(
                 (result) => {
